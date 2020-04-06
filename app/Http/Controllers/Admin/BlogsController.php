2fb8048721e3f88e6c4ec46e7d10093223e9a686
ashikur3700 <asikur.rahman@eongroup.net.bd>
2020-04-07 01:12:17 +0600

    updated

---
+++ app/Http/Controllers/Admin/BlogsController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+
+use App\Blog;
+use Illuminate\Http\Request;
+use DataTables;
+use Illuminate\Support\Facades\URL;
+class BlogsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+
+            if ($request->ajax()) {
+            $data = Blog::latest()->get();
+                return Datatables::of($data)
+                        ->addIndexColumn()
+                        ->addColumn('action', function($row){
+
+                               $btn = '<div class="btn-group"><a href="'.URL::to('/').'/admin/blogs/'.$row->id.'/edit" class="btn btn-sm btn-outline-primary">Edit</a>
+                               <button onclick="deleteData('.$row->id.')" class="btn btn-sm btn-outline-danger">Delete</button></div>';
+
+                                return $btn;
+                        })
+
+                        ->rawColumns(['action'])
+                        ->escapeColumns([])
+                        ->make(true);
+
+                   }
+     
+                    return view('admin.blogs.index');
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('admin.blogs.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+			'title' => 'required|max:10'
+		]);
+        $requestData = $request->all();
+        
+        Blog::create($requestData);
+
+        return redirect('admin/blogs')->with('flash_message', 'Blog added!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $blog = Blog::findOrFail($id);
+
+        return view('admin.blogs.show', compact('blog'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $blog = Blog::findOrFail($id);
+
+        return view('admin.blogs.edit', compact('blog'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+			'title' => 'required|max:10'
+		]);
+        $requestData = $request->all();
+        
+        $blog = Blog::findOrFail($id);
+        $blog->update($requestData);
+
+        return redirect('admin/blogs')->with('flash_message', 'Blog updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy($id)
+    {
+        Blog::destroy($id);
+
+        return redirect('admin/blogs')->with('flash_message', 'Blog deleted!');
+    }
+}
---
+++ app/Http/Controllers/Admin/BookController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+
+use App\Book;
+use Illuminate\Http\Request;
+use DataTables;
+use Illuminate\Support\Facades\URL;
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+
+            if ($request->ajax()) {
+            $data = Book::latest()->get();
+                return Datatables::of($data)
+                        ->addIndexColumn()
+                        ->addColumn('action', function($row){
+
+                               $btn = '<div class="btn-group"><a href="'.URL::to('/').'/admin/book/'.$row->id.'/edit" class="btn btn-sm btn-outline-primary">Edit</a>
+                               <button onclick="deleteData('.$row->id.')" class="btn btn-sm btn-outline-danger">Delete</button></div>';
+
+                                return $btn;
+                        })
+
+                        ->rawColumns(['action'])
+                        ->escapeColumns([])
+                        ->make(true);
+
+                   }
+     
+                    return view('admin.book.index');
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('admin.book.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+			'title' => 'required|max:10'
+		]);
+        $requestData = $request->all();
+        
+        Book::create($requestData);
+
+        return redirect('admin/book')->with('flash_message', 'Book added!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $book = Book::findOrFail($id);
+
+        return view('admin.book.show', compact('book'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $book = Book::findOrFail($id);
+
+        return view('admin.book.edit', compact('book'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+			'title' => 'required|max:10'
+		]);
+        $requestData = $request->all();
+        
+        $book = Book::findOrFail($id);
+        $book->update($requestData);
+
+        return redirect('admin/book')->with('flash_message', 'Book updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy($id)
+    {
+        Book::destroy($id);
+
+        return redirect('admin/book')->with('flash_message', 'Book deleted!');
+    }
+}
